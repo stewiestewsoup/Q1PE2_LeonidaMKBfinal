5d6ebb34b6302a7e3abde7536223fc4748c88d74
stewiestewsoup <143673362+stewiestewsoup@users.noreply.github.com>
2024-09-18 11:20:41 +0800

    Add files via upload

---
+++ Q1PE2_LeonidaMKBfinal/src/q1pe2_leonidamkbfinal/Q1PE2_LeonidaMKBfinal.java
@@ -0,0 +1,152 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package q1pe2_leonidamkbfinal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author PC7-COMLAB1
+ */
+public class Q1PE2_LeonidaMKBfinal {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+    Scanner num = new Scanner(System.in);
+    System.out.print("Enter a binary string: ");
+    int no = num.nextInt();
+            if (no == 1) {
+                System.out.print("One ");
+            } else if (no == 10) {
+                System.out.print("Two ");
+            } else if (no == 3) {
+                System.out.print("Three ");
+            } else if (no == 100) {
+                System.out.print("Four ");
+            } else if (no == 101) {
+                System.out.print("Five ");
+            } else if (no == 110) {
+                System.out.print("Six ");
+            } else if (no == 111) {
+                System.out.print("Seven ");
+            } else if (no == 1000) {
+                System.out.print("Eight ");
+            } else if (no == 1001) {
+                System.out.print("Nine ");
+            } else if (no == 1010) {
+                System.out.print("Ten ");
+            } else if (no == 1011) {
+                System.out.print("Eleven ");
+            } else if (no == 1100) {
+                System.out.print("Twelve ");
+            } else if (no == 1101) {
+                System.out.print("Thirteen ");
+            } else if (no == 1110) {
+                System.out.print("Fourteen ");
+            } else if (no == 1111) {
+                System.out.print("Fifteen ");
+            } else if (no == 10000) {
+                System.out.print("Sixteen ");
+            } else if (no == 10001) {
+                System.out.print("Seventeen ");
+            } else if (no == 10010) {
+                System.out.print("Eighteen ");
+            } else if (no == 10011) {
+                System.out.print("Nineteen ");
+            } else if (no == 10100) {
+                System.out.print("Twenty ");
+            } else if (no == 10101) {
+                System.out.print("Twenty-One ");
+            } else if (no == 10110) {
+                System.out.print("Twenty-Two ");
+            } else if (no == 10111) {
+                System.out.print("Twenty-Three ");
+            } else if (no == 11000) {
+                System.out.print("Twenty-Four ");
+            } else if (no == 11001) {
+                System.out.print("Twenty-Five ");
+            } else if (no == 11010) {
+                System.out.print("Twenty-Six ");
+            } else if (no == 11011) {
+                System.out.print("Twenty-Seven ");
+            } else if (no == 11100) {
+                System.out.print("Twenty-Eight ");
+            } else if (no == 11101) {
+                System.out.print("Twenty-Nine ");
+            } else if (no == 11110) {
+                System.out.print("Thirty ");
+            } else if (no == 11111) {
+                System.out.print("Thirty-One ");
+            } else if (no == 100000) {
+                System.out.print("Thirty-Two ");
+            } else if (no == 100001) {
+                System.out.print("Thirty-Three ");
+            } else if (no == 100010) {
+                System.out.print("Thirty-Four ");
+            } else if (no == 100011) {
+                System.out.print("Thirty-Five ");
+            } else if (no == 100100) {
+                System.out.print("Thirty-Six ");
+            } else if (no == 100101) {
+                System.out.print("Thirty-Seven ");
+            } else if (no == 100110) {
+                System.out.print("Thirty-Eight ");
+            } else if (no == 100111) {
+                System.out.print("Thirty-Nine ");
+            } else if (no == 101000) {
+                System.out.print("Forty ");
+            } else if (no == 101001) {
+                System.out.print("Forty-One ");
+            } else if (no == 101010) {
+                System.out.print("Forty-Two ");
+            } else if (no == 101011) {
+                System.out.print("Forty-Three ");
+            } else if (no == 101100) {
+                System.out.print("Forty-Four ");
+            } else if (no == 101101) {
+                System.out.print("Forty-Five ");
+            } else if (no == 101110) {
+                System.out.print("Forty-Six ");
+            } else if (no == 101111) {
+                System.out.print("Forty-Seven ");
+            } else if (no == 110000) {
+                System.out.print("Forty-Eight ");
+            } else if (no == 110001) {
+                System.out.print("Forty-Nine ");
+            } else if (no == 110010) {
+                System.out.print("Fifty ");
+            } else if (no == 110011) {
+                System.out.print("Fifty-One ");
+            } else if (no == 110100) {
+                System.out.print("Fifty-Two ");
+            } else if (no == 110101) {
+                System.out.print("Fifty-Three ");
+            } else if (no == 110110) {
+                System.out.print("Fifty-Four ");
+            } else if (no == 110111) {
+                System.out.print("Fifty-Five ");
+            } else if (no == 111000) {
+                System.out.print("Fifty-Six ");
+            } else if (no == 111001) {
+                System.out.print("Fifty-Seven ");
+            } else if (no == 111010) {
+                System.out.print("Fifty-Eight ");
+            } else if (no == 111011) {
+                System.out.print("Fifty-Nine ");
+            } else if (no == 111100) {
+                System.out.print("Sixty ");
+            } else if (no == 111101) {
+                System.out.print("Sixty-One ");
+            } else if (no == 111110) {
+                System.out.print("Sixty-Two ");
+            } else if (no == 111111) {
+                System.out.print("Sixty-Three ");
+            } else if (no == 000000) {
+                System.out.print("Zero ");
+            } 
+    }
+}
